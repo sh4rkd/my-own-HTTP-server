6a67dbd121283f0c38cc8850cd79b03974dc979d
sh4rkd <developer@fredmiramontes.dev>
2024-07-21 19:33:35 -0600

    Feat: create my own HTTP server

---
+++ app/main.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+
+// Uncomment this to pass the first stage
+const server = net.createServer((socket) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n")
+
+  socket.on("close", () => {
+    socket.end();
+  });
+});
+
+server.listen(4221, "localhost");
